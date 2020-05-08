51585a115625e7cb2faa9888cb42d92bfafa8928
Bharath3697 <bharath458.bk@gmail.com>
2020-05-09 00:09:54 +0530

    added homepage

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -7,6 +7,9 @@ app.get('/', function (req, res) {
 app.get('/mars', function(req, res) {
   res.send('Hellooo Marssss!\n'); 
 });
+app.get('/home', function(req, res) {
+  res.send('welcome!\n'); 
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
